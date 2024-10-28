d5b46f02b93cb311218501f29b8c96369f834819
setPedro <pedross4789@gmail.com>
2024-10-28 22:07:38 +0100

    get more tweets

--- src/app/api/tweets/route.ts
+++ src/app/api/tweets/route.ts
@@ -1,15 +1,34 @@
 export async function POST(req: Request) {
   const { userId } = await req.json();
-
   const URL = `https://api.socialdata.tools/twitter/user/${userId}/tweets`;
+  const headers = {
+    Authorization: `Bearer ${process.env.SOCIALDATA_API_KEY}`,
+    Accept: "application/json",
+  };
 
-  const res = await fetch(URL, {
-    headers: {
-      Authorization: `Bearer ${process.env.SOCIALDATA_API_KEY}`,
-      Accept: "application/json",
-    },
-  });
+  const tweets = [];
+  let cursor = null;
 
-  const data = await res.json();
-  return new Response(JSON.stringify(data.tweets));
+  try {
+    while (tweets.length < 63) {
+      const response: any = await fetch(
+        `${URL}${cursor ? `?cursor=${cursor}` : ""}`,
+        { headers },
+      );
+
+      const data = await response.json();
+      if (!data.tweets || data.tweets.length === 0) break;
+
+      tweets.push(...data.tweets);
+      cursor = data.next_cursor;
+      if (!cursor) break;
+    }
+
+    return new Response(JSON.stringify(tweets), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching tweets:", error);
+    return new Response(JSON.stringify({ error: "Failed to fetch tweets" }), {
+      status: 500,
+    });
+  }
 }
